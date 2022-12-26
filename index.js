2e0ab300c425d42443649f50e19b10288b5a458f
Ramiz Ali <ramiz.apk7@gmail.com>
2022-12-26 05:14:04 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+//test 1
+function introduction(name){
+    return `Hi, my name is ${name}.`;
+}
+//test 2
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+//test 3 & 4
+function introductionWithLanguageOptional(name, language="JavaScript"){
+     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
